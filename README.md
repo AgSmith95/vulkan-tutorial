ca3ecafc843474f3f49803e773e3dd4c93d591e6
AgSmith95 <ivan.a.kornienko@gmail.com>
2023-07-24 00:07:08 +0300

    Drawing a triangle
    Setup
      Base code
      Instance

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Me trying to learn Vulkan API
+
+Resources:
+1. https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Instance
--- main.cpp
+++ main.cpp
@@ -1,36 +1,142 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+#include <cstring>
 
-int main() {
-	glfwInit();
+using std::cout;
+using std::runtime_error;
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window =
-		glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+static constexpr uint32_t WIDTH = 800;
+static constexpr uint32_t HEIGHT = 600;
 
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+class HelloTriangleApplication {
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+private:
+	void initWindow() {
+		glfwInit();
 
-	std::cout << extensionCount << " extensions supported\n";
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	glm::vec4 test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan-Hello-Triangle", nullptr, nullptr);
 	}
 
-	glfwDestroyWindow(window);
+	void initVulkan() {
+		createInstance();
+	}
 
-	glfwTerminate();
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
 
-	std::cout << test[0] << " END!\n";
-	return 0;
+	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+		// appInfo.pNext = nullptr; // automatically set to nullptr because extension is unused
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.ppEnabledExtensionNames = glfwGetRequiredInstanceExtensions(&createInfo.enabledExtensionCount);
+		createInfo.enabledLayerCount = 0;
+
+		if (!extensionRequirementsSatisfiedInstance(createInfo.enabledExtensionCount, createInfo.ppEnabledExtensionNames)) {
+			throw runtime_error("!createInstance: !extensionRequirementsSatisfiedInstance");
+		}
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw runtime_error("!createInstance: vkCreateInstance != VK_SUCCESS");
+		}
+	}
+
+	bool extensionRequirementsSatisfiedInstance(uint32_t numRequiredExtensions, const char* const *requiredExtensionsNames) {
+		#ifdef DEBUG
+		cout << "DEBUG: Required extensions for creating VkInstance:\n";
+		for (uint32_t i = 0; i < numRequiredExtensions; ++i) {
+			cout << '\t' << requiredExtensionsNames[i] << '\n';
+		}
+		#endif // DEBUG
+		if (numRequiredExtensions == 0) { // no requirements
+			return true;
+		}
+
+		// get extensions count
+		uint32_t numSupportedExtensions = 0;
+		VkResult res = vkEnumerateInstanceExtensionProperties(nullptr, &numSupportedExtensions, nullptr);
+		if (res != VK_SUCCESS) {
+			throw runtime_error("!extensionRequirementsSatisfiedInstance: vkEnumerateInstanceExtensionProperties != VK_SUCCESS");
+		}
+		if (numSupportedExtensions < numRequiredExtensions) {
+			return false;
+		}
+		// populate extensions array
+		std::vector<VkExtensionProperties> supportedExtensions(numSupportedExtensions);
+		res = vkEnumerateInstanceExtensionProperties(nullptr, &numSupportedExtensions, supportedExtensions.data());
+		if (res != VK_SUCCESS) {
+			throw runtime_error("!extensionRequirementsSatisfiedInstance: vkEnumerateInstanceExtensionProperties != VK_SUCCESS");
+		}
+		#ifdef DEBUG
+		cout << "DEBUG: Supported Vulkan extensions for VkInstance:";
+		for (const VkExtensionProperties& extension: supportedExtensions) {
+			cout << '\t' << extension.extensionName << '\n';
+		}
+		#endif // DEBUG
+
+		// check if all required are supported
+		for (uint32_t i = 0; i < numRequiredExtensions; ++i) {
+			const char* const requiredExtensionName = requiredExtensionsNames[i];
+			bool found = false;
+			for (const VkExtensionProperties& extension: supportedExtensions) {
+				if (strcmp(extension.extensionName, requiredExtensionName) == 0) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	GLFWwindow *window;
+	VkInstance instance;
+};
+
+int main(int /*argc*/, const char** /*argv*/) {
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
